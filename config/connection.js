49c803c9cc131f76748326896342c459e7533468
ash-sp-cole <sk8er9238@hotmail.com>
2019-04-26 15:33:50 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,7 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.ASHSP_URL){
-    connection = mysql.createConnection(process.env.ASHSP_URL);
-  }
-  else{
+{
     connection = mysql.createConnection({
       host     : 'localhost',
       user     : 'root',
