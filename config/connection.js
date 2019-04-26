cf96bbe3cb6eae81c69378fc950d4a0e2fa0dc6e
ash-sp-cole <sk8er9238@hotmail.com>
2019-04-26 15:45:55 -0400

    t

--- config/connection.js
+++ config/connection.js
@@ -19,13 +19,13 @@
   };
   
   // Make the connection to MySQL
-  connection.connect(function(err) {
-    if (err) {
-      console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
-      return;
-    }
-    console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
-  });
+  // connection.connect(function(err) {
+  //   if (err) {
+  //     console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+  //     return;
+  //   }
+  //   console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  // });
   
   // Export connection for ORM use
   module.exports = connection;
