f46281590b3918097659407b5aba9fa72e21cfd9
ash-sp-cole <sk8er9238@hotmail.com>
2019-04-26 16:44:04 -0400

    final

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,30 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Codepass7",
-    database: "burgers_db"
-  });
+// Create the MySQL connection object
+var connection;
 
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'Codepass7',
+		database: 'burgers_db'
+	})
+};
 
+// Make the connection to MySQL
 connection.connect(function(err) {
-  if (err) throw err;
- 
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-//exports this file content so other files can access it
+// Export connection for ORM use
 module.exports = connection;
-
